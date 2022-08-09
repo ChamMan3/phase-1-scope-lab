0ab175f0e922fd918f4bd8df91ef3b8511ed10e8
LukeChamley <lkchamley@gmail.com>
2022-08-09 13:56:02 -0600

    commit tree a79e333d518394c32913c7908a555a06ebe548da

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const LeastFavoriteCustomer = "randy"
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'alfred'
+}
